679dbb4392e62958d98054d08daa5d14c395f86a
andersonpales <andersonpales2010@gmail.com>
2021-10-06 16:17:11 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,17 @@
+var swiper = new Swiper(".mySwiper", {
+    spaceBetween: 30,
+    effect: "fade",
+    centeredSlides: true,
+    autoplay: {
+        delay: 8000,
+      disableOnInteraction: false,
+  },
+  pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+  },
+  navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+  },
+});
