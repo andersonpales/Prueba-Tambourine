5c3249bf2aa2b5ddd59983a586b291316b19f7bf
andersonpales <andersonpales2010@gmail.com>
2021-10-07 12:09:10 -0500

    Add files via upload

--- script.js
+++ script.js
@@ -15,3 +15,40 @@ var swiper = new Swiper(".mySwiper", {
       prevEl: ".swiper-button-prev",
   },
 });
+
+/* ----- Booknow ---- */
+
+function openNav() {
+  document.getElementById("mySidenav").style.width = "300px";
+  document.getElementById("main").style.marginRight = "300px";
+}
+
+function closeNav() {
+  document.getElementById("mySidenav").style.width = "0";
+  document.getElementById("main").style.marginRight= "0";
+}
+
+
+/* ------- Form ------- */
+
+var currentDateTime = new Date();
+var year = currentDateTime.getFullYear();
+var month = (currentDateTime.getMonth() + 1);
+var date = (currentDateTime.getDate() + 1);
+
+if(date < 10) {
+  date = '0' + date;
+}
+if(month < 10) {
+  month = '0' + month;
+}
+
+var dateTomorrow = year + "-" + month + "-" + date;
+var checkinElem = document.querySelector("#checkin-date");
+var checkoutElem = document.querySelector("#checkout-date");
+
+checkinElem.setAttribute("min", dateTomorrow);
+
+checkinElem.onchange = function () {
+    checkoutElem.setAttribute("min", this.value);
+}
